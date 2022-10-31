21037ea08319ac784143d6ea4d2c2bf155257c45
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-31 18:34:33 +0800

    Fix validations of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
