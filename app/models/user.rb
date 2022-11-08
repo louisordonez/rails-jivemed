7c2829d96a8f51b3927170f16ed78d48e3b2090d
grenzk <74033991+grenzk@users.noreply.github.com>
2022-11-08 19:46:47 +0800

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_secure_password
   has_and_belongs_to_many :roles
+  has_many :appointments
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -14,8 +15,17 @@ class User < ApplicationRecord
             uniqueness: true
   validates :email_verified, inclusion: [true, false]
   validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :stripe_id, presence: true
 
   before_validation :set_default
+  before_validation :create_on_stripe, on: :create
+
+  def create_on_stripe
+    params = { email: email, name: "#{first_name} #{last_name}" }
+    response = Stripe::User.create(params)
+
+    self.stripe_id = response.id
+  end
 
   private
 
