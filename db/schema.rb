c39babd8ee150a2bc0dae3a0463f2adf711d3164
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-08 20:11:08 +0800

    Add user stripe_id col

--- db/schema.rb
+++ db/schema.rb
@@ -72,6 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_085544) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "stripe_id"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
