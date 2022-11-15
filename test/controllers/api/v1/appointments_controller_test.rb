a79dbc6e722e5489ec852088ef6643872cd735d3
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-15 14:09:08 +0800

    Generate appointments

--- test/controllers/api/v1/appointments_controller_test.rb
+++ test/controllers/api/v1/appointments_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Api::V1::ReservationsControllerTest < ActionDispatch::IntegrationTest
+class Api::V1::AppointmentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
