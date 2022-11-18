ed8e4742b2f39cffe72d364cd7042f7c8d845b38
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-18 22:16:04 +0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,6 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = {
-    host: 'example.com',
-    protocol: 'https://'
-  }
-
   # Gmail Config
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
