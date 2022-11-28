9a83c9a6e8cbd462a76ab9548e1dec22f778aa6d
louisordonez <72607877+louisordonez@users.noreply.github.com>
2022-11-28 19:58:38 +0800

    Update jivemed_mailer.rb

--- app/mailers/jivemed_mailer.rb
+++ app/mailers/jivemed_mailer.rb
@@ -4,7 +4,7 @@ class JivemedMailer < ApplicationMailer
   def confirm_email
     @user = params[:user]
     @email_token = params[:email_token]
-    @url = "#{ENV['JIVEMED_EMAIL_URL']}#{@email_token}"
+    @url = "#{ENV['JIVEMED_CONFIRM_EMAIL_URL']}#{@email_token}"
     mail(to: @user.email, subject: 'Welcome to Jivemed!')
   end
 end
